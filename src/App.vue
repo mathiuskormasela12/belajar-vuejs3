ee9b074bf6815753a3f78a55fad04ab112139121
Mathius <mathiuskormasela12@gmail.com>
2022-05-15 16:46:26 +0700

    Belajar V-Model, Conditinal Rendering dan Looping

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,45 @@
 <template>
-  <div>
-    <h1>Belajar Vue Js</h1>
-    <!-- Mencetak data -->
-    <p :title="nama">Hi my name is {{ nama }}</p>
-    <p>Di Klik {{ count }}</p>
-    <!-- Memanggil method -->
-    <button type="button" @click="increment">Count</button>
-    <button type="button" @click="count--">Decrement</button>
-  </div>
+  <h1>Belajar Vue Js</h1>
+  <p>Value nya adalah {{ title }}</p>
+  <!-- Shorthand -->
+  <input type="text" placeholder="Type the movie title" v-model="title" />
+  <!-- <input
+    type="text"
+    placeholder="Type the movie title"
+    :value="title"
+    @input="title = $event.target.value"
+  /> -->
+  <!-- Membuat conditional rendering -->
+  <p v-if="title === 'naruto'">Naruto Siampudan</p>
+  <p v-else-if="title === 'boruto'">Boruto Siakkangan</p>
+  <p v-else>404</p>
+
+  <p>Skill saya</p>
+  <ol>
+    <li v-for="(item, index) in skills" :key="item.id">
+      {{ index }} - {{ item.name }}
+    </li>
+  </ol>
 </template>
 
 <script>
+/*
+    ================= Belajar V-Model, Conditional Rendering & Looping =================
+
+    A. V-Model
+       V-Model merupakan berungsi untuk mengubah nilai
+       dari sebuah data menggunakan sebuah
+       element input HTML.
+*/
 export default {
-  /*
-    ============ Belajar Declarative Rendering & Methods ============
-    
-    A. Data
-       Data merupakan sebuah variable seperti 
-       state dalam React Js. Ketika value data nya
-       berubah, otomatis componentnya akan
-       di render ulang.
-
-    B. Declarative Rendering
-       Declarative rendering merupakan cara kita
-       untuk mencetak nilai dari data.
-
-    C. Methods
-       Method merupakan kumpulan function atau event
-       yang dapat di panggil.
-  */
   data() {
     return {
-      nama: "Mathius",
-      count: 0,
+      title: "",
+      skills: [
+        { id: 1, name: "Node Js" },
+        { id: 2, name: "Vue Js" },
+      ],
     };
-  },
-
-  methods: {
-    increment() {
-      this.count += 1;
-    },
   },
 };
 </script>
---
+++ src/Pertemuan1.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <h1>Belajar Vue Js</h1>
+    <!-- Mencetak data -->
+    <p :title="nama">Hi my name is {{ nama }}</p>
+    <p>Di Klik {{ count }}</p>
+    <!-- Memanggil method -->
+    <button type="button" @click="increment">Count</button>
+    <button type="button" @click="count--">Decrement</button>
+  </div>
+</template>
+
+<script>
+export default {
+  /*
+    ============ Belajar Declarative Rendering & Methods ============
+    
+    A. Data
+       Data merupakan sebuah variable seperti 
+       state dalam React Js. Ketika value data nya
+       berubah, otomatis componentnya akan
+       di render ulang.
+
+    B. Declarative Rendering
+       Declarative rendering merupakan cara kita
+       untuk mencetak nilai dari data.
+
+    C. Methods
+       Method merupakan kumpulan function atau event
+       yang dapat di panggil.
+  */
+  data() {
+    return {
+      nama: "Mathius",
+      count: 0,
+    };
+  },
+
+  methods: {
+    increment() {
+      this.count += 1;
+    },
+  },
+};
+</script>
