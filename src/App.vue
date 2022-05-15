e8d7d46825b682e04ff43a73c5f5377a8d5c15e4
Mathius <mathiuskormasela12@gmail.com>
2022-05-15 21:32:15 +0700

    Belajar Membuat Component

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,20 @@
 <template>
-  <h1>Belajar Vue Js</h1>
-  <p>Value nya adalah {{ title }}</p>
-  <!-- Shorthand -->
-  <input type="text" placeholder="Type the movie title" v-model="title" />
-  <!-- <input
-    type="text"
-    placeholder="Type the movie title"
-    :value="title"
-    @input="title = $event.target.value"
-  /> -->
-  <!-- Membuat conditional rendering -->
-  <p v-if="title === 'naruto'">Naruto Siampudan</p>
-  <p v-else-if="title === 'boruto'">Boruto Siakkangan</p>
-  <p v-else>404</p>
-
-  <p>Skill saya</p>
-  <ol>
-    <li v-for="(item, index) in skills" :key="item.id">
-      {{ index }} - {{ item.name }}
-    </li>
-  </ol>
+  <div>
+    <h1>Belajar Looping Dengan Component</h1>
+    <my-title v-for="num in numbers" :key="num" :title="num" />
+  </div>
 </template>
 
 <script>
-/*
-    ================= Belajar V-Model, Conditional Rendering & Looping =================
+import MyTitle from "./components/MyTitle.vue";
 
-    A. V-Model
-       V-Model merupakan berungsi untuk mengubah nilai
-       dari sebuah data menggunakan sebuah
-       element input HTML.
-*/
 export default {
+  components: {
+    MyTitle,
+  },
   data() {
     return {
-      title: "",
-      skills: [
-        { id: 1, name: "Node Js" },
-        { id: 2, name: "Vue Js" },
-      ],
+      numbers: [1, 2, 3],
     };
   },
 };
---
+++ src/components/MyTitle.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Hello {{ title }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      default: "",
+    },
+  },
+};
+</script>
