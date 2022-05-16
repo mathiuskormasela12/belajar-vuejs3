43faa5c7a7518ed8bbabf7d692878babdfe9cf7f
Mathius <mathiuskormasela12@gmail.com>
2022-05-16 20:44:28 +0700

    pertemuan 5

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,37 @@
 <template>
   <div>
-    <h1>Lifecycle</h1>
+    <h1>V-Once</h1>
+    <!-- Hanya akan di render sekalih -->
+    <!-- <p v-once>{{ value }}</p> -->
+    <!-- Merender data sebagai html -->
+    <p v-html="value"></p>
+    <input type="text" v-model="value" />
+    <p>{{ cetak }}</p>
+    <p>Ini bre {{ print }}</p>
+    <button @click="print = 'Test'">Test</button>
   </div>
 </template>
 
 <script>
 export default {
-  // dijalankna ketika component sblm di buat (blm di tempel)
-  beforeCreate() {
-    console.log("beforeCreate");
+  data() {
+    return {
+      value: "lama",
+    };
   },
 
-  // di jalankan ketika component sudah di buat (blm di tempel)
-  created() {
-    console.log("created");
-  },
-
-  // dijalankan sebelm component di tempel (setelah component di buat)
-  beforeMount() {
-    console.log("beforeMount");
-  },
-
-  // dijalankan sesudah component di tempel (setelah component di buat)
-  mounted() {
-    console.log("mounted");
-  },
-
-  // di jalankan sebelum ketika ada perubahan data dan props
-  beforeUpdate() {
-    console.log("beforeUpdate");
-  },
-
-  // di jalankan ketika ada perubahan data dan props
-  updated() {
-    console.log("updated");
-  },
-
-  //  dijakankan sebelum component di hapus
-  beforeUnmount() {
-    console.log("beforeUnmount");
-  },
-
-  // di jalankan ketika component sudah di hapus
-  unmounted() {
-    console.log("unmounted");
+  computed: {
+    cetak() {
+      return "Hello WOrld";
+    },
+    print: {
+      get() {
+        return this.value;
+      },
+      set(val) {
+        this.value = val.toUpperCase();
+      },
+    },
   },
 };
 </script>
---
+++ src/Pertemuan4.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <h1>Lifecycle</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  // dijalankna ketika component sblm di buat (blm di tempel)
+  beforeCreate() {
+    console.log("beforeCreate");
+  },
+
+  // di jalankan ketika component sudah di buat (blm di tempel)
+  created() {
+    console.log("created");
+  },
+
+  // dijalankan sebelm component di tempel (setelah component di buat)
+  beforeMount() {
+    console.log("beforeMount");
+  },
+
+  // dijalankan sesudah component di tempel (setelah component di buat)
+  mounted() {
+    console.log("mounted");
+  },
+
+  // di jalankan sebelum ketika ada perubahan data dan props
+  beforeUpdate() {
+    console.log("beforeUpdate");
+  },
+
+  // di jalankan ketika ada perubahan data dan props
+  updated() {
+    console.log("updated");
+  },
+
+  //  dijakankan sebelum component di hapus
+  beforeUnmount() {
+    console.log("beforeUnmount");
+  },
+
+  // di jalankan ketika component sudah di hapus
+  unmounted() {
+    console.log("unmounted");
+  },
+};
+</script>
